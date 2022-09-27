629c7d1172424613efdaee5ebd6077802748969b
Dev-buddy05 <114466895+Dev-buddy05@users.noreply.github.com>
2022-09-27 11:58:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title("Devendra Data Scientist")
+st.write("Msc DSAI :sunglasses:")
+st.header("Python")
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
+st.caption('Explains above equation')
+code = '''def hello():
+    print("Hello, Data Scientist Devendra!")'''
+st.code(code, language='python')
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
